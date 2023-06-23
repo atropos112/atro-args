f30f2d42c5a72ea9d3e6be016ef00a70e89e7eba
atropos <sv7n@pm.me>
2023-06-23 18:26:47 +0100

    Arg parse atro version.

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="atro-pylog",
-    version="0.0.5",
+    name="atro-pyparser",
+    version="0.0.1",
     packages=find_packages(),
     author="Atropos",
     author_email="pypi.rising@atro.xyz",
-    description="A simple logging library for Python. That either uses Loguru or OpenTelmetry collector (useful for local & cluster logging).",
+    description="A simple parsing wrapper around argparse. It works with atro-pylog otherwise its just about the same.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/atropos/atro-pylog",
-    install_requires=["loguru", "opentelemetry-api", "opentelemetry-exporter-otlp-proto-grpc", "opentelemetry-sdk", "pydantic", "python-dotenv"],
+    install_requires=[
+        "atro-pylog",
+        "argparse",
+    ],
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
