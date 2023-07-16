5074254c0a0cecbac94bbe73c244db81e45c3632
atropos <sv7n@pm.me>
2023-07-16 22:47:17 +0100

    Poetry and updated pylog

--- atro_args/__init__.py
+++ atro_args/__init__.py
@@ -1,18 +1,13 @@
 import argparse
+import logging
 import sys
-
-from pylog import set_logger
 
 
 class PyParser(argparse.ArgumentParser):
     def __init__(self, *args, **kwargs):
-        if "logger" in kwargs:
-            self.logger = kwargs.pop("logger")
-        else:
-            self.logger = set_logger()
         super().__init__(*args, **kwargs)
 
     def error(self, message):
-        self.logger.error(message)
+        logging.error(message)
         self.print_help()
         sys.exit(2)
